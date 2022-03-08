2c7fab967b0ef9dc643d45a9797cccb78643a5fc
drbess <drbess@aggies.ncat.edu>
2022-03-07 23:05:03 -0500

    Files from Module 2

---
+++ module-2/name-io.py
@@ -0,0 +1,5 @@
+# A basic input-process-output program usng the the "print" function
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("Your name is: " + fnam + " "+ lnam)
