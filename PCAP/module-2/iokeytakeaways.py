5239bd8b7d5303ca77c33313e989c7340fb11da5
roycecom <devin@roycecom.io>
2023-09-17 20:52:44 -0400

    update 1: removed the PCAP folder

--- PCAP/module-2/iokeytakeaways.py
+++
@@ -1,8 +0,0 @@
-# The input function can be used to end a program
-
-name = input("Enter your name: ")
-print("Hello, " + name + ". Nice to meet you!")
-
-print("\nPress Enter to end the program.")
-input()
-print("THE END.")
